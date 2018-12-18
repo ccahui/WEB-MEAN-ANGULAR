9b1f558a302fd2ce57bac2138e4497995b601270
ccahui <kris_ti4n@hotmail.com>
2018-12-18 00:06:22 -0500

    a

--- src/app/paginas/tablero1/tablero1.component.ts
+++ src/app/paginas/tablero1/tablero1.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tablero1.component.css']
 })
 export class Tablero1Component implements OnInit {
-
+  // tslint:disable-next-line:no-inferrable-types
+  porcentaje: number = 50;
   constructor() { }
 
   ngOnInit() {
+  }
+  cambiarPorcentaje(porcentaje: number) {
+    if (porcentaje > 100) {
+      this.porcentaje = 100;
+      return;
+    }
+    if (porcentaje < 0) {
+      this.porcentaje = 0;
+    }
+    this.porcentaje = this.porcentaje + porcentaje;
   }
 
 }
