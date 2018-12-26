fa0f284e2b066d39b367399cc7f20457a7d5dc50
ccahui <kris_ti4n@hotmail.com>
2018-12-25 22:55:42 -0500

    Login con LocalStoreage, token, recuerdame

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { Usuario } from '../modelos/usuario.model';
+import { UsuarioService } from '../servicios/servicio.index';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +10,38 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css', './default-dark.css', './bootstrap.min.css', './style.css']
 })
 export class LoginComponent implements OnInit {
+  email: string;
+  recuerdame: boolean;
 
-  constructor( public router: Router) { }
+  constructor(public usuarioService: UsuarioService, public router: Router) {
+    this.recuerdame = false;
+    this.email = '';
+  }
 
   ngOnInit() {
+
+    if (localStorage.getItem('email')) {
+      this.email = localStorage.getItem('email');
+      this.recuerdame = true;
+    } else {
+      this.email = '';
+      this.recuerdame = false;
+    }
+
   }
-  ingresar() {
-    console.log('Ingresando...');
-    // this.router.navigate(['/tablero']);
-    window.location.assign('tablero');
+  ingresar(formulario: NgForm) {
+
+    if (formulario.invalid) {
+      return;
+    }
+
+    const usuario = new Usuario(null, formulario.value.email, formulario.value.password);
+    this.usuarioService.login(usuario, this.recuerdame)
+      .subscribe(estado => {
+        // this.router.navigate(['/tablero']); Recordad el problema de los scripts 
+        window.location.assign('tablero');
+
+      });
+
   }
 }
--- src/app/servicios/usuario/usuario.service.ts
+++ src/app/servicios/usuario/usuario.service.ts
@@ -21,4 +21,28 @@ export class UsuarioService {
         })
       );
   }
+  // Recuedame es opcional recuerdame?boolean
+  login(usuario: Usuario, recuedame = false) {
+
+    const url = URL_SERVICE + '/login';
+    if (recuedame === true) { // El recuerdame esta activo
+      localStorage.setItem('email', usuario.email);
+    } else {
+      // Si esta desactivo se remueve
+      localStorage.removeItem('email');
+    }
+
+
+    return this.http.post(url, usuario)
+      .pipe(
+        map((res: any) => {
+          // Almacenando en el LocalStorage
+          localStorage.setItem('id', res.id);
+          localStorage.setItem('token', res.token);
+          // Recordad que res.USUARIO es un Objeto, y localStorage solo acepta String
+          localStorage.setItem('usuario', JSON.stringify(res.usuario));
+          return true;
+        })
+      );
+  }
 }
